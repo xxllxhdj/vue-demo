2efd931473a81eb285642b86f536c5ebea12830d
xuxle <xuxle@yonyou.com>
2018-03-07 13:44:55 +0800

    Test: BkgColor

--- src/test/views/DirectiveView.vue
+++ src/test/views/DirectiveView.vue
@@ -4,12 +4,18 @@
             <h4>基础指令</h4>
             <div v-demo></div>
         </div>
+        <div class="test-card">
+            <h4 v-bkg-color="color">指令测试</h4>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'directiveView'
+        name: 'directiveView',
+        data: function () {
+            return { color: '#00ff00' };
+        }
     }
 </script>
 
